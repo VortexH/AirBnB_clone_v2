3aaba0e4ff23d3142ad7c2c726d2f7bce90b74ca
VortexH <hemant.heer@gmail.com>
2019-01-14 23:56:13 +0000

    Adds sudo in front of commands because servers don't run as root

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,13 @@
 # Installs nginx, creates folders, and updates nginx to serve content from one folder
 sudo apt-get update
 sudo apt-get -y install nginx
-mkdir -p "/data/web_static/releases/test"
-mkdir -p "/data/web_static/shared/"
-touch /data/web_static/releases/test/index.html
-echo "Testing this file" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown --recursive ubuntu:ubuntu /data/
-wget https://raw.githubusercontent.com/VortexH/AirBnB_clone_v2/master/third_config
-cp third_config /etc/nginx/sites-available/default
+sudo mkdir -p "/data/web_static/releases/test"
+sudo mkdir -p "/data/web_static/shared/"
+sudo touch /data/web_static/releases/test/index.html
+echo "Testing this file" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown --recursive ubuntu:ubuntu /data/
+sudo wget https://raw.githubusercontent.com/VortexH/AirBnB_clone_v2/master/third_config
+sudo cp third_config /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo service nginx start
+sudo service nginx restart
