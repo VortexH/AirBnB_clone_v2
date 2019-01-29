74fded665d8e22c6be4eb857caf908f511652b13
VortexH <hemant.heer@gmail.com>
2019-01-29 02:37:48 +0000

    Created web_flask directory, populated it with: init.py and 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Imports an instance of flask from __init__.py and creates a route """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ function to handle the default / route
+
+        Returns:
+                a generic string
+    """
+    return "Hello HBNB!"
